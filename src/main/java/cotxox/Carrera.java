d04d244a1159f6be27d1dcecbe8e908d953b20f4
unknown <patezno97@gmail.com>
2019-02-09 18:44:16 +0100

    added attributes on Carrera

--- src/main/java/cotxox/Carrera.java
+++ src/main/java/cotxox/Carrera.java
@@ -1,4 +1,10 @@
 package cotxox;
 
 public class Carrera {
+
+    private int tiempoEsperado = 0;
+    private int tiempoCarrera = 0;
+    private double costeTotal = 0;
+    private String conductor = "";
+
 }
