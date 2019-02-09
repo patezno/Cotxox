c81560f6e57058c01899074e2c72cc2b91d8ffa9
unknown <patezno97@gmail.com>
2019-02-09 18:58:45 +0100

    created all methods for Carrera

--- src/main/java/cotxox/Carrera.java
+++ src/main/java/cotxox/Carrera.java
@@ -2,9 +2,39 @@ package cotxox;
 
 public class Carrera {
 
+    // Atributos
+
     private int tiempoEsperado = 0;
     private int tiempoCarrera = 0;
     private double costeTotal = 0;
     private String conductor = "";
 
+    // Metodos
+
+    public String getTarjetaCredito() {
+    }
+
+    public String getOrigen() {
+    }
+
+    public String getDestino() {
+    }
+
+    public String getDistancia() {
+    }
+
+    public String getCosteEsperado() {
+    }
+
+    public void realizarPago(String costeEsperado) {
+    }
+
+    public void asignarConductor(PoolConductores conductores) {
+    }
+
+    public void recibirPropina(int i) {
+    }
+
+    public void liberarConductor() {
+    }
 }
