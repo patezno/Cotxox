53058ab074a088174db46b771cc18b3252ca3010
unknown <patezno97@gmail.com>
2019-02-10 18:11:17 +0100

    started tarifa class

--- src/main/java/cotxox/Tarifa.java
+++ src/main/java/cotxox/Tarifa.java
@@ -1,4 +1,23 @@
 package cotxox;
 
 public class Tarifa {
+
+    // Atributos
+
+    private double costeMilla = 1.35;
+    private double costeMinuto = 0.35;
+    private int costeMinimo = 5;
+    private double porcentajeComision = 20;
+
+    // Metodos
+
+    public double getCosteDistancia(Carrera carrera) {
+        return carrera.getDistancia() * costeMilla;
+    }
+
+    public double getCosteTiempo(Carrera carrera) {
+        return carrera.getTiempoCarrera() * costeMinuto;
+    }
+    
+
 }
